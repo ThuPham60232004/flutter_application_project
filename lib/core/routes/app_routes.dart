647b421b48e445c9440f177b6648f2436ab54759
Suzi161099 <nlht1610@gmail.com>
2024-12-27 20:16:07 +0700

    updatesignup_signin

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_project/presentation/screens/home_screen.dart';
 import 'package:flutter_application_project/presentation/screens/login_screen.dart';
 
-import 'package:flutter_application_project/presentation/signup_screen.dart';
+import 'package:flutter_application_project/presentation/screens/signup_screen.dart';
 import 'route_names.dart';
 
 class AppRoutes {
